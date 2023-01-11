5110341870e3629a8c9a3d7a757153aba5b8b946
emopedia <76584247+emopedia@users.noreply.github.com>
2023-01-11 19:56:30 +1300

    Create Sockets.kt

---
+++ src/main/kotlin/io/github/stealbrew/plugins/Sockets.kt
@@ -0,0 +1,29 @@
+package io.github.stealbrew.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+
+fun Application.configureSockets() {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    routing {
+        webSocket("/ws") {
+            // handle incoming text frames
+            incoming.filterIsInstance<Frame.Text>().map { it.readText() }
+                .filter { text -> text.equals("bye", ignoreCase = true) }
+                .onEach { text -> outgoing.send(Frame.Text("YOU SAID: $text")) }
+                .onEach { close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE")) }
+        }
+    }
+}
