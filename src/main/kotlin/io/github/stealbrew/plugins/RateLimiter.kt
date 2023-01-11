e7e8775aef1138e1fa81b0b5d03daa06f00101e0
emopedia <76584247+emopedia@users.noreply.github.com>
2023-01-11 19:58:43 +1300

    Update RateLimiter.kt

--- src/main/kotlin/io/github/stealbrew/plugins/RateLimiter.kt
+++ src/main/kotlin/io/github/stealbrew/plugins/RateLimiter.kt
@@ -1,1 +1,23 @@
+package io.github.stealbrew.plugins
 
+import io.github.stealbrew.utils.requestPrincipal
+import io.ktor.server.application.*
+import io.ktor.server.plugins.ratelimit.*
+import kotlin.time.seconds
+
+fun Application.configureRateLimiter() {
+    install(RateLimit) {
+        global {
+            requestKey { call ->
+                call.requestPrincipal ?: PrincipalResult.Unauthorized
+            }
+
+            rateLimiter { _, key ->
+                when (key) {
+                    is PrincipalResult.Authorized -> RateLimiter.Unlimited
+                    else -> RateLimiter.default(30, 30.seconds)
+                }
+            }
+        }
+    }
+}
